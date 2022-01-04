d65dd9d99dbc725ea38afba39132122cb9701c43
Abdirizak Omer <abdirizakomer188@gmail.com>
2022-01-04 17:34:28 +0300

    add

---
+++ index.js
@@ -0,0 +1,80 @@
+const bas = [
+    {
+    name: 'Xasan',
+    price: 2000
+    },
+    {
+    name: 'Xamda',
+    price: 2000
+    }, 
+    {
+    name: 'Caasha',
+    price: 0
+    },
+    {name: 'Nuur',
+    price: 1500
+    },
+    {
+    name: 'Maxamed',
+    price: 1000
+    }, 
+    {
+    name: 'Jamaal',
+    price: 2000
+    }
+];
+
+
+const shop = [
+    {
+        item: 'Kaluun',
+        price: 9000,
+        quantity: 10
+    },
+    {
+        item: 'Mongo Sharaab',
+        price: 5000,
+        quantity: 8
+    },
+    {
+        item: 'Caano Maraaci',
+        price: 5000,
+        quantity: 20
+    },
+    {
+        item: 'Caano Lis',
+        price: 6000,
+        quantity: 24
+    },
+    {
+        item: 'Fanta',
+        price: 3500,
+        quantity: 50
+    },
+    {
+        item: 'Buskud Cake',
+        price: 1000,
+        quantity: 0
+    },{
+        item: 'Saliid',
+        price: 80000,
+        quantity: 1
+    },{
+        item: 'Jawaan Sonkor',
+        price: 120000,
+        quantity: 0
+    }
+]
+
+
+let prices2000Names = {
+
+    name: 'mohamed',
+    price: 2000
+    
+};
+console.log(prices2000Names)
+
+Filter
+Reduce 
+Map 
